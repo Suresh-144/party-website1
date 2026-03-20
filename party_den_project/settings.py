8386a4d43359b1d48b8d4ad77f07a9fdba5e0244
Suresh-144 <yenugulasuresh144@gmail.com>
2026-03-20 18:47:36 +0530

    Configure WhiteNoise for static file handling
    Added WhiteNoise middleware and updated static files settings.

--- party_den_project/settings.py
+++ party_den_project/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -98,6 +100,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (Uploaded Gallery Photos)
 MEDIA_URL = '/media/'
